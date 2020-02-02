676fc8ee37beeb5f6e0c7c151c50b5518c958ff8
brandonschultz <baschultz2842@eagle.fgcu.edu>
2020-02-02 18:31:31 -0500

    adjusted sizing so it doesnt change size

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App(props) {
 
   return (
     <div className="Home">
-     <img src={home} className='image_style' alt="oops" height="700" width="100%"/>
-     <div className="sign_text" width="auto">
+     {/* <img src={home} className='image_style' alt="oops" height="700" width="100%"/> */}
+     <div className="sign_text">
       {/* <p>18 Ridge View Rd <br></br> {days} days <br></br>by Cornerstone Homes </p> */}
       <p id='street_address'>18 Ridge View Rd</p>
       <p id='time_text'>{days} days</p>
