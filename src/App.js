c5e16f7278989d035a01c98d1f167faa46969d61
brandonschultz <baschultz2842@eagle.fgcu.edu>
2020-02-02 16:33:33 -0500

    text adjusts when resizing screen

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App(props) {
   return (
     <div className="Home">
      <img src={home} className='image_style' alt="oops" height="700" width="100%"/>
-     <div className="sign_text">
+     <div className="sign_text" width="auto">
       {/* <p>18 Ridge View Rd <br></br> {days} days <br></br>by Cornerstone Homes </p> */}
       <p id='street_address'>18 Ridge View Rd</p>
       <p id='time_text'>{days} days</p>
